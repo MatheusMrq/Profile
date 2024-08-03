202695c51df6a83a4cd16d1383f71f9cfd59b349
MatheusMrq <146729866+MatheusMrq@users.noreply.github.com>
2024-08-03 14:55:21 -0300

    Update.

---
+++ Scripts/script.js
@@ -0,0 +1,27 @@
+// Theme Trader
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggle = document.getElementById('theme-toggle');
+    const currentTheme = localStorage.getItem('theme') || 'light';
+
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    themeToggle.addEventListener('click', () => {
+        let theme = document.documentElement.getAttribute('data-theme');
+        if (theme === 'dark') {
+            document.documentElement.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light');
+            themeToggle.textContent = '🌙';
+        } else {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+            themeToggle.textContent = '🌞';
+        }
+    });
+});
+
+// Fade in (Theme-Button)
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    this.classList.remove('fade-in');
+    void this.offsetWidth; 
+    this.classList.add('fade-in');
+});
